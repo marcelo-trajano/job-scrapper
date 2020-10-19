e0fd64624ce3bf51d8febd9fe9e1176aa16115db
Marcelo <marcelo.trajano@outlook.com>
2020-10-19 20:34:00 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import App2 from "./App2";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
-);
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App2 />
-  </React.StrictMode>,
-  document.getElementById("root1")
 );
