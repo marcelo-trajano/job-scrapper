0091b33249f61b3cf8aa283d857eacf00d0ebe3e
Marcelo <marcelo.trajano@outlook.com>
2020-10-20 00:29:27 -0300

    ٩(◕‿◕)۶

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,132 +1,127 @@
-(async () => {
-  const express = require("express");
-  //const cors = require("cors");
-  const mongoose = require("mongoose");
-  const puppeteer = require("puppeteer");
-  const Positions = require("./Positions");
-  const SearchTerm = require("./SearchTerm");
-  const moment = require("moment");
-  const app = express();
-  require("dotenv").config();
-  //app.use(cors({ origin: process.env.CORS_ORIGIN }));
-  await mongoose.connect(
-    "mongodb+srv://userdb:Dev19821@cluster0.znl6l.mongodb.net/userdb?retryWrites=true&w=majority",
-    { useNewUrlParser: true }
-  );
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const puppeteer = require("puppeteer");
+const Positions = require("./Positions");
+const SearchTerm = require("./SearchTerm");
+const moment = require("moment");
+const app = express();
+require("dotenv").config();
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
-  app.use((req, res, next) => {
-    console.log("ACESSANDO MIDDLEWARE...");
+// app.use((req, res, next) => {
+//   console.log("ACESSANDO MIDDLEWARE...");
 
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-Requested-With,content-type"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
+//   res.setHeader("Access-Control-Allow-Credentials", true);
 
-    next();
-  });
+//   next();
+// });
 
-  app.get("/positions/", async (req, res) => {
-    try {
-      const data = Number(moment(Date.now()).format("YYYYMMDD"));
-      let list = [];
-      if (req.query.search) {
-        if (req.query.salary === "true") {
-          list = await Positions.find({
-            code: { $eq: req.query.search },
-            salary: { $ne: null },
-          });
-        } else {
-          list = await Positions.find({
-            code: { $eq: req.query.search },
-            insertDate: { $eq: data },
-          });
-        }
-        res.json(list);
+app.get("/positions/", async (req, res) => {
+  try {
+    const data = Number(moment(Date.now()).format("YYYYMMDD"));
+    let list = [];
+    if (req.query.search) {
+      if (req.query.salary === "true") {
+        list = await Positions.find({
+          code: { $eq: req.query.search },
+          salary: { $ne: null },
+        });
       } else {
-        res.json(list);
+        list = await Positions.find({
+          code: { $eq: req.query.search },
+          insertDate: { $eq: data },
+        });
       }
-    } catch (error) {
-      console.log(error);
-      res.json(error);
+      res.json(list);
+    } else {
+      res.json(list);
     }
-  });
+  } catch (error) {
+    console.log(error);
+    res.json(error);
+  }
+});
 
-  app.get("/getAllSearchTerms", async (req, res) => {
-    try {
-      let result = [];
-      const data = Number(moment(Date.now()).format("YYYYMMDD"));
+app.get("/getAllSearchTerms", async (req, res) => {
+  try {
+    let result = [];
+    const data = Number(moment(Date.now()).format("YYYYMMDD"));
 
-      result = await SearchTerm.find({
-        //createdAt: new Date("2020-10-15T05:11:21.957+00:00"),
-        //createdAt: { $gte: "2020-10-14", $lte: "2020-10-15" },
-        insertDate: { $eq: data },
-      });
-
-      res.json(result);
-    } catch (error) {
-      console.log(error);
-    }
-  });
-
-  app.get("/positions/detail/", async (req, res) => {
-    try {
-      let result = [];
-
-      if (req.query.link) {
-        result = await getjobDetail(req.query.link);
-        res.json(result.result);
-      } else {
-        res.json(result);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  });
-
-  const getjobDetail = async (link) => {
-    return new Promise(async (resolve, reject) => {
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
-      await page.goto(link);
-
-      let result = await page.evaluate(async () => {
-        let list = [];
-        document.querySelectorAll("#jobDescriptionText").forEach((b) => {
-          list.push(b.innerText);
-        });
-        document.querySelectorAll("#jobDescriptionText p").forEach((b) => {
-          list.push(b.innerText);
-        });
-        document.querySelectorAll("#jobDescriptionText ul li").forEach((b) => {
-          list.push(b.innerText);
-        });
-        return list;
-      });
-      page.close();
-      result = result.join(" ");
-      resolve({ result });
+    result = await SearchTerm.find({
+      //createdAt: new Date("2020-10-15T05:11:21.957+00:00"),
+      //createdAt: { $gte: "2020-10-14", $lte: "2020-10-15" },
+      insertDate: { $eq: data },
     });
-  };
 
-  app.get("/positions/getAll/:search", async (req, res) => {
-    try {
-      let result = [];
-      result = await Positions.find({ code: { $eq: req.params.search } });
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/positions/detail/", async (req, res) => {
+  try {
+    let result = [];
+
+    if (req.query.link) {
+      result = await getjobDetail(req.query.link);
+      res.json(result.result);
+    } else {
       res.json(result);
-    } catch (error) {
-      console.log(error);
-      res.json({ error });
     }
-  });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-  const PORT = process.env.PORT || 8789;
-  app.listen(PORT, () => {
-    console.log("server listening at port: " + PORT);
+const getjobDetail = async (link) => {
+  return new Promise(async (resolve, reject) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(link);
+
+    let result = await page.evaluate(async () => {
+      let list = [];
+      document.querySelectorAll("#jobDescriptionText").forEach((b) => {
+        list.push(b.innerText);
+      });
+      document.querySelectorAll("#jobDescriptionText p").forEach((b) => {
+        list.push(b.innerText);
+      });
+      document.querySelectorAll("#jobDescriptionText ul li").forEach((b) => {
+        list.push(b.innerText);
+      });
+      return list;
+    });
+    page.close();
+    result = result.join(" ");
+    resolve({ result });
   });
-})();
+};
+
+app.get("/positions/getAll/:search", async (req, res) => {
+  try {
+    let result = [];
+    result = await Positions.find({ code: { $eq: req.params.search } });
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.json({ error });
+  }
+});
+
+const PORT = process.env.PORT || 8789;
+app.listen(PORT, () => {
+  console.log("server listening at port: " + PORT);
+});
